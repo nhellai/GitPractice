61d549bd76971abf4a94cf3e3067f2eb236000e2
hellai <hnoorzai@ymail.com>
2022-11-22 16:55:02 -0500

    origin

--- src/gitPracticePackage/MyGitClass.java
+++ src/gitPracticePackage/MyGitClass.java
@@ -3,5 +3,11 @@ package gitPracticePackage;
 public class MyGitClass {
 public static void main(String[] args) {
 	System.out.println("This is test");
+	System.out.println("This is test");
+	System.out.println("This is test");
+	System.out.println("This is test");
+	System.out.println("This is test");
+	System.out.println("This is test");
+	System.out.println("This is test");
 }
 }

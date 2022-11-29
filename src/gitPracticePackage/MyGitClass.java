599202a6628cdb8adaa5b5119f5b58e23be7e1f7
hellai <hnoorzai@ymail.com>
2022-11-29 09:19:41 -0500

    next commit

--- src/gitPracticePackage/MyGitClass.java
+++ src/gitPracticePackage/MyGitClass.java
@@ -9,5 +9,7 @@ public static void main(String[] args) {
 	System.out.println("This is test");
 	System.out.println("This is test");
 	System.out.println("This is test");
+	System.out.println("This is test");
+	System.out.println("This is a new test for Git");
 }
 }

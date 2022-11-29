f6c3cd41f161ed06a46953104db934095e4cd7b8
nhellai <118852901+nhellai@users.noreply.github.com>
2022-11-29 09:53:23 -0500

    Update MyGitClass.java
    This is from the remote repo

--- src/gitPracticePackage/MyGitClass.java
+++ src/gitPracticePackage/MyGitClass.java
@@ -11,5 +11,10 @@ public static void main(String[] args) {
 	System.out.println("This is test");
 	System.out.println("This is test");
 	System.out.println("This is a new test for Git");
+	
+	
+	
+	
+	System.out.println("This changes were made directly from the GitHub");
 }
 }
